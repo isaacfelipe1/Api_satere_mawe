4bedb2c37689c77f786ece1393d2604133d1081f
isaacFelipe1 <ifdsl.lic20@uea.edu.br>
2023-10-17 03:35:24 -0300

    primeira versão

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, render_template
 import mysql.connector
 app = Flask(__name__)
+app.secret_key = 'isaacfelipe30@'
 db = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -10,6 +11,26 @@ db = mysql.connector.connect(
 @app.route('/')
 def inicial():
     return render_template("index.html")
+@app.route('/documentacao')
+def documentacao():
+    return render_template("documentacao.html")
+
+@app.route('/api/numeros_satere', methods=['GET'])
+def listar_numeros_satere_mawe():
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM numeros_descricoes")
+    numeros = cursor.fetchall()
+    cursor.close()
+    numero_json = []
+    for numero in numeros:
+        numero_dict = {
+            "id": numero[0],
+            "numero": numero[1],
+            "descricao": numero[2]
+        }
+        numero_json.append(numero_dict)
+    return jsonify(numero_json)
+
 @app.route('/api/vogais', methods=['GET'])
 def listar_vogais():
     cursor = db.cursor()
@@ -24,15 +45,12 @@ def listar_vogais():
             "descricao": vogal[2]
         }
         vogais_json.append(vogal_dict)
-
     return jsonify(vogais_json)
-
-# ...
-@app.route('/api', methods=['GET'])
-def listar_numeros_satere_mawe():
+@app.route('/api/numeros_de_trezentos', methods=['GET'])
+def listar_numeros_satere_trezentos():
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM numeros_descricoes")
-    numeros= cursor.fetchall()
+    cursor.execute("SELECT * FROM numeros_de_trezentos")
+    numeros = cursor.fetchall()
     cursor.close()
     numero_json = []
     for numero in numeros:
@@ -42,20 +60,25 @@ def listar_numeros_satere_mawe():
             "descricao": numero[2]
         }
         numero_json.append(numero_dict)
-
     return jsonify(numero_json)
 
 @app.route('/api/alfabeto', methods=['GET'])
 def listar_alfabeto():
     cursor = db.cursor()
-    cursor.execute("SELECT letra FROM alfabeto")
+    cursor.execute("SELECT id, letra FROM alfabeto")
     letras = cursor.fetchall()
     cursor.close()
-
-    alfabeto_json = [{"letra": letra[0]} for letra in letras]
-
+    alfabeto_json = [{"id": letra[0], "letra": letra[1]} for letra in letras]
     return jsonify(alfabeto_json)
 
+@app.route('/api/palavras', methods=['GET'])
+def listar_palavras():
+    cursor = db.cursor()
+    cursor.execute("SELECT id, palavra, satere FROM palavras_satere")
+    palavras = cursor.fetchall()
+    cursor.close()
+    palavras_json = [{"id": palavra[0], "palavra": palavra[1], "satere": palavra[2]} for palavra in palavras]
+    return jsonify(palavras_json)
 
 if __name__ == '__main__':
     app.run(debug=True)
